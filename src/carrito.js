407addbc5203d034916c0ccade931b229c352886
LuliMovio <luisina.movio@gmail.com>
2026-07-25 20:47:42 -0300

    Updated

--- src/carrito.js
+++ src/carrito.js
@@ -1,1 +1,21 @@
+class Carrito {
+    constructor() {
+        this.productos = [];
+    }
 
+    agregarProducto(producto) {
+        this.productos.push(producto);
+    }
+
+    calcularTotal() {
+        let total = 0;
+
+        this.productos.forEach(producto => {
+            total += producto.precio * producto.cantidad;
+        });
+
+        return total;
+    }
+}
+
+module.exports = Carrito;
