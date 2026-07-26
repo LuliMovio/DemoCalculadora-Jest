dba3a0f62c6ec5e94100eb151b791364491eda88
Nadia Berrino <nanuzaghis20@gmail.com>
2026-07-25 21:12:37 -0300

    agrego test del carrito

--- tests/carrito.test.js
+++ tests/carrito.test.js
@@ -1,1 +1,23 @@
+const Carrito = require('../src/carrito');
 
+describe('Pruebas unitarias para Carrito de Compras', () => {
+    let carrito;
+
+    beforeEach(() => {
+        carrito = new Carrito();
+    });
+
+    test('Debe agregar un producto correctamente y calcular el total (Unitaria + Assertions)', () => {
+        carrito.agregarProducto({ nombre: 'Remera', precio: 1000, cantidad: 2 });
+        
+        expect(carrito.productos).toHaveLength(1);
+        expect(carrito.calcularTotal()).toBe(2000);
+    });
+
+    test('Debe calcular el total con múltiples productos', () => {
+        carrito.agregarProducto({ nombre: 'Remera', precio: 1000, cantidad: 1 });
+        carrito.agregarProducto({ nombre: 'Pantalón', precio: 3000, cantidad: 2 });
+
+        expect(carrito.calcularTotal()).toBe(7000);
+    });
+});
